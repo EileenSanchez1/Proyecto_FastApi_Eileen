d2ac1637857bc7dc820a075c812aed6d0a332465
EileenSanchez1 <eileensanchez49@gmail.com>
2026-05-27 22:07:12 -0500

    modelo: actualizar campos del modelo de transacciones

---
+++ app/models/transacciones.py
@@ -0,0 +1,9 @@
+from pydantic import BaseModel
+
+class TransaccionCrear(BaseModel):
+    valor_unitario: float
+    cantidad: int
+    factura_id: int
+
+class Transaccion(TransaccionCrear):
+    id: int
