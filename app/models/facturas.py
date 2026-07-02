c3f859984d9dc4699b9e61dd51003e0670485e6e
EileenSanchez1 <eileensanchez49@gmail.com>
2026-07-01 22:28:27 -0500

    feat: actualiza modelo de facturas

--- app/models/facturas.py
+++ app/models/facturas.py
@@ -4,9 +4,11 @@ from sqlmodel import SQLModel, Field
 
 
 class Factura(SQLModel, table=True):
+    __tablename__ = "facturas"
+
     id: Optional[int] = Field(default=None, primary_key=True)
     fecha: datetime = Field(default_factory=datetime.now)
-    cliente_id: int
+    cliente_id: int = Field(foreign_key="clientes.id")
 
     @property
     def valor_total(self) -> float:
