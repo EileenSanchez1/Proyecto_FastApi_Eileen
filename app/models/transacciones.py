6d20154c1c01ee282ae7bd6dd745c2275222cb14
EileenSanchez1 <eileensanchez49@gmail.com>
2026-07-01 22:28:30 -0500

    feat: actualiza modelo de transacciones

--- app/models/transacciones.py
+++ app/models/transacciones.py
@@ -3,7 +3,9 @@ from sqlmodel import SQLModel, Field
 
 
 class Transaccion(SQLModel, table=True):
+    __tablename__ = "transacciones"
+
     id: Optional[int] = Field(default=None, primary_key=True)
     valor_unitario: float
     cantidad: int
-    factura_id: int
+    factura_id: int = Field(foreign_key="facturas.id")
