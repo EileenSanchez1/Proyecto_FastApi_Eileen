626319ad1790ff56f40c1a3eb296a74889aea534
EileenSanchez1 <eileensanchez49@gmail.com>
2026-07-01 22:28:23 -0500

    feat: actualiza modelo de clientes

--- app/models/clientes.py
+++ app/models/clientes.py
@@ -3,6 +3,8 @@ from sqlmodel import SQLModel, Field
 
 
 class Cliente(SQLModel, table=True):
+    __tablename__ = "clientes"
+
     id: Optional[int] = Field(default=None, primary_key=True)
     nombre: str
     edad: int
